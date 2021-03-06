be21a5b6a8b09b62a86ec1b5dde1e950ef00fb9c
Iryna Prendkovych <iryna.prendkovych.itis.2020@lpnu.ua>
2021-03-06 20:43:11 +0200

    OOP lab 1.5

---
+++ UnitTest1.5/UnitTest1.5.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab 1.5/Pair.h"
+#include "../lab 1.5/Pair.cpp"
+#define TESTING
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest15
+{
+	TEST_CLASS(UnitTest15)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Pair c;
+			c.SetA(2);
+			int test = c.GetA();
+			Assert::AreEqual(test, 2);
+
+		}
+	};
+}
